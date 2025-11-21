ea7110258904cc8d8b1dd1ceb3894ac13e12eadc
Sudarashan Sahu <134515668+Sudarashan@users.noreply.github.com>
2025-11-21 18:36:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -317,5 +317,5 @@ def delete_post(post_index):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Get the port from Render
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
